f3df994b5f2fbcf963191946ee2c1aa3ff128346
Earn <earndarkness@perfenterprise.com>
2019-07-24 16:49:30 +0700

    Initialize project

---
+++ SimpleUITest/AppInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace SimpleUITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                //return ConfigureApp.Android.StartApp();
+
+                return ConfigureApp.Android
+                                   //.ApkFile("../../../demoUITest/demoUITest/demoUITest.Android/bin/Debug/com.companyname.demoUITest.apk")
+                                   //.InstalledApp("com.companyname.demoUITest")
+                                   .ApkFile(@"C:\Users\earnd\source\repos\demoUITest\demoUITest\demoUITest.Android\bin\Debug\com.companyname.demoUITest.apk")
+                                   //.DeviceSerial("21e2d61e")
+                                   .StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ demoUITest/demoUITest/Views/WebviewPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace demoUITest.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WebviewPage : ContentPage
+    {
+        public WebviewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
